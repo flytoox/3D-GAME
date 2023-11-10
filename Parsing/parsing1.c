4a5615c2fa506c1526d925b560bfe5f01e31d2d3
DA3NAN <adnane.aitmalek20@gmail.com>
2023-11-10 11:05:32 +0100

    printing map on screen

--- Parsing/parsing1.c
+++ Parsing/parsing1.c
@@ -6,7 +6,7 @@
 /*   By: aait-mal <aait-mal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 10:10:20 by aait-mal          #+#    #+#             */
-/*   Updated: 2023/11/10 10:11:10 by aait-mal         ###   ########.fr       */
+/*   Updated: 2023/11/10 11:04:16 by aait-mal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@ bool	is_itclosed(char **map, int n, int x, int y)
 {
 	if (x < 0 || y < 0 || x >= (int)ft_strlen(map[y]) || y >= n)
 		return (false);
-	if (map[y][x] == '1' || map[y][x] == 'x')
+	if (map[y][x] == '1' || map[y][x] == 'x' || map[y][x] == 'n')
 		return (true);
 	if (map[y][x] == '0' || map[y][x] == 'N')
 	{
-		map[y][x] = 'x';
+		if (map[y][x] == 'N')
+			map[y][x] = 'n';
+		else
+			map[y][x] = 'x';
 		if (is_itclosed(map, n, x + 1, y)
 			&& is_itclosed(map, n, x - 1, y)
 			&& is_itclosed(map, n, x, y + 1)
@@ -42,6 +45,53 @@ void	print_map(char **map)
 	}
 }
 
+void draw_filled_rectangle(void *mlx_ptr, void *win_ptr, int x, int y, int width, int height, int color)
+{
+    int i, j;
+
+    i = 0;
+    while (i < height)
+    {
+        j = 0;
+        while (j < width)
+        {
+            mlx_pixel_put(mlx_ptr, win_ptr, x + j, y + i, color);
+            j++;
+        }
+        i++;
+    }
+}
+
+void	display_map_on_screen(char **map)
+{
+	int		i;
+	int		j;
+	t_mlx	*mlx;
+	int square_size;
+
+	mlx = malloc(sizeof(t_mlx));
+	mlx->mlx_ptr = mlx_init();
+	mlx->win_ptr = mlx_new_window(mlx->mlx_ptr, WIDTH, HEIGHT, "CUB3D");
+	square_size = WIDTH / ft_strlen(map[0]);
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == '1')
+				draw_filled_rectangle(mlx->mlx_ptr, mlx->win_ptr, j * square_size, i * square_size, square_size, square_size, 0x00FF0000);
+			else if (map[i][j] == 'x')
+				draw_filled_rectangle(mlx->mlx_ptr, mlx->win_ptr, j * square_size, i * square_size, square_size, square_size, 0x00FFFFFF);
+			else if (map[i][j] == 'n')
+				draw_filled_rectangle(mlx->mlx_ptr, mlx->win_ptr, j * square_size, i * square_size, square_size, square_size, 0x0000FF00);
+			j++;
+		}
+		i++;
+	}
+	mlx_loop(mlx->mlx_ptr);
+}
+
 bool	check_closed(char **map, int n)
 {
 	int	x;
@@ -61,6 +111,7 @@ bool	check_closed(char **map, int n)
 		}
 		y++;
 	}
+	display_map_on_screen(map);
 	return (1);
 }
 
